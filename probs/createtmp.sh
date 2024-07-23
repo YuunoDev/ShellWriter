c9e79849e81c72e6864f3925a585bc402257b84b
YuunoDev <adrianalonso.a4@proton.me>
2024-07-23 17:55:05 -0600

    chore: Update createtmp.sh to create temporary file in tmp directory

--- probs/createtmp.sh
+++ probs/createtmp.sh
@@ -1,4 +1,31 @@
-#! /bin/bash
+#!/bin/bash
+filename=$1
+
+# Extract the base name
+base="${filename%.*}"
+
+# Extract the extension
+ext="${filename##*.}"
 
 # Create a temporary file
-temp_file=$(mktemp /tmp/pruebaTemporal.XXXXXX)
+temp_file=$(mktemp tmp/$base.XXXXXX)
+
+# Check if the file was created successfully
+if [[ ! -z "$temp_file" ]]; then
+  echo "Temporary file created: $temp_file"
+else
+  echo "Failed to create temporary file"
+  exit 1
+fi
+
+# Use the temporary file for your operations
+# For example, write some data to it
+cat "$1" > "$temp_file"
+
+# Display the contents of the temporary file
+cat "$temp_file"
+
+# Clean up by removing the temporary file
+sleep 10
+rm "$temp_file"
+echo "Temporary file removed."
--- probs/offset.sh
+++ probs/offset.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 # Path: probs/offset.sh
-
 # Desplazarte en el offset en incrementos de 2
 texto="Hola, mundo"
-offset=0
+offset=9
 le=1
-
 echo $texto | awk -v offset=$offset -v le=$le '{print substr($0, offset, le)}'
-
